9366af99616f866e02e1103f42a242ec659b634c
rajivjajo <rajivjajodia82552@gmail.com>
2023-01-24 20:07:02 +0530

    Welcome to multiple companies employee details program!

---
+++ src/com/bridgelabz/workshop2/CompanyMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.workshop2;
+
+public class CompanyMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to multiple companies Database!");
+    }
+}
